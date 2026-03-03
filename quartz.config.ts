7417f7baba7e1890c17f1c844407b6e2612e6dd6
Mitra Martin <mitra@oxygentango.com>
2026-03-03 15:32:20 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "You Have a Raiment",
+    pageTitle: "You Have A Raiment",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
